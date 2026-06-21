a08d51d32ad3f704e19b5d2047604c2db2694751
RoyTeheuxZuyd <2506589teheux@zuyd.nl>
2026-06-21 09:12:33 +0200

    melding page update
    melding page ingevuld met contactgegevens om melding te maken

---
+++ MatrixIncBezorgApp/Services/MeldingStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MatrixIncBezorgApp.Services
+{
+    internal class MeldingStore
+    {
+        public static string WarehouseName { get; set; } = "Magazijn";
+
+        public static string WarehouseEmail { get; set; } = "magazijn@matrixinc.nl";
+
+        public static string WarehousePhone { get; set; } = "+31611111111";
+
+        public static string HelpdeskName { get; set; } = "Helpdesk";
+
+        public static string HelpdeskEmail { get; set; } = "helpdesk@matrixinc.nl";
+
+        public static string HelpdeskPhone { get; set; } = "+31622222222";
+    }
+}
